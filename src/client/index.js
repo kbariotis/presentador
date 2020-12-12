ddbc188e75351261b01cc25b6c37d07a3c110a13
kbariotis <konmpar@gmail.com>
2020-12-12 19:19:27 +0200

    fix click on controls

--- src/client/index.js
+++ src/client/index.js
@@ -30,39 +30,51 @@ page({
 });
 
 document.getElementsByTagName("body")[0].onkeyup = (event) => {
+  event.preventDefault();
+
   if (event.key === "ArrowRight") {
     document
       .getElementsByClassName("arrow-right")[0]
       .classList.remove("active");
-  }
-  if (event.key === "ArrowLeft") {
-    document.getElementsByClassName("arrow-left")[0].classList.remove("active");
-  }
-};
-document.getElementsByTagName("body")[0].onkeydown = (event) => {
-  if (event.key === "ArrowRight") {
-    document.getElementsByClassName("arrow-right")[0].classList.add("active");
     page(`/${currentId + 1}`);
   }
   if (event.key === "ArrowLeft") {
-    document.getElementsByClassName("arrow-left")[0].classList.add("active");
+    document.getElementsByClassName("arrow-left")[0].classList.remove("active");
     page(`/${currentId - 1}`);
+  }
+};
+document.getElementsByTagName("body")[0].onkeydown = (event) => {
+  event.preventDefault();
+
+  if (event.key === "ArrowRight") {
+    document.getElementsByClassName("arrow-right")[0].classList.add("active");
+  }
+  if (event.key === "ArrowLeft") {
+    document.getElementsByClassName("arrow-left")[0].classList.add("active");
   }
 };
 
 const handleClick = (event) => {
-  const vw = Math.max(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  const median = vw / 2;
+  event.preventDefault();
 
-  if (event.clientX > median) {
-    document.getElementsByClassName("arrow-right")[0].classList.add("active");
-    page(`/${currentId + 1}`);
-  } else {
-    document.getElementsByClassName("arrow-left")[0].classList.add("active");
-    page(`/${currentId - 1}`);
+  const bodyEl = document.getElementsByClassName("slides")[0];
+
+  if (bodyEl.contains(event.target)) {
+    const vw = Math.max(
+      document.documentElement.clientWidth || 0,
+      window.innerWidth || 0
+    );
+    const median = vw / 2;
+
+    if (event.clientX > median) {
+      document.getElementsByClassName("arrow-right")[0].classList.add("active");
+      page(`/${currentId + 1}`);
+    } else {
+      document.getElementsByClassName("arrow-left")[0].classList.add("active");
+      page(`/${currentId - 1}`);
+    }
+
+    return;
   }
 };
 
@@ -71,8 +83,21 @@ const clearAfterClick = () => {
   document.getElementsByClassName("arrow-right")[0].classList.remove("active");
 };
 
-document.onmousedown = handleClick;
-document.ontouchstart = handleClick;
+document.getElementsByClassName("arrow-right")[0].onclick = (event) => {
+  event.preventDefault();
+  document.getElementsByClassName("arrow-right")[0].classList.add("active");
+  page(`/${currentId + 1}`);
+  return;
+};
+document.getElementsByClassName("arrow-left")[0].onclick = (event) => {
+  event.preventDefault();
+  document.getElementsByClassName("arrow-left")[0].classList.add("active");
+  page(`/${currentId - 1}`);
+  return;
+};
 
-document.ontouchend = clearAfterClick;
-document.onmouseup = clearAfterClick;
+document.getElementsByTagName("body")[0].onmousedown = handleClick;
+document.getElementsByTagName("body")[0].ontouchstart = handleClick;
+
+document.getElementsByTagName("body")[0].ontouchend = clearAfterClick;
+document.getElementsByTagName("body")[0].onmouseup = clearAfterClick;
