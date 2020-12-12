ffe388c15dd1ff7a78f58b822754c7cac9e36fa6
kbariotis <konmpar@gmail.com>
2020-12-12 13:05:39 +0200

    home page

--- playground/index.js
+++ playground/index.js
@@ -1,42 +1,4 @@
-const compiler = require("../src/compiler");
-const codeMirror = require("codemirror");
-
-require("codemirror/lib/codemirror.css");
-require("codemirror/theme/base16-light.css");
-
 require("./index.scss");
-require(`../src/client/index.scss`);
 
-const params = new URLSearchParams(document.location.search);
-
-const codemirror = codeMirror(
-  document.getElementsByClassName("playground-editor")[0],
-  {
-    mode: "markdown",
-    lineNumbers: true,
-    lineWrapping: true,
-    autofocus: true,
-    value:
-      params.get("content") ||
-      `
-## Presentation title
-
-##### Description of the presentation
-`,
-  }
-);
-
-codemirror.on("change", (instance) => showResults(instance.getValue()));
-
-const showResults = (source) => {
-  const { html, state } = compiler(source);
-  require(`../src/renderers/${state}.scss`);
-  document.getElementsByClassName(
-    "playground-results"
-  )[0].innerHTML = `<div class="${state}">${html}</div>`;
-
-  params.set("content", source);
-  window.history.replaceState(null, null, `?${params.toString()}`);
-};
-
-showResults(codemirror.getValue());
+require("../src/client/index.scss");
+require("../src/renderers/headerList.scss");
--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -3,7 +3,10 @@ const path = require("path");
 
 module.exports = {
   mode: process.env.WEPBACK_MODE || "development",
-  entry: path.resolve(`${__dirname}/index.js`),
+  entry: {
+    playground: path.resolve(`${__dirname}/playground.js`),
+    index: path.resolve(`${__dirname}/index.js`),
+  },
   module: {
     rules: [
       {
@@ -17,11 +20,18 @@ module.exports = {
     ],
   },
   output: {
-    filename: "index.bundle.js",
+    filename: "[name].js",
   },
   plugins: [
     new HtmlWebpackPlugin({
+      template: path.resolve(`${__dirname}/playground.html`),
+      filename: "playground.html",
+      chunks: ["playground"],
+    }),
+    new HtmlWebpackPlugin({
       template: path.resolve(`${__dirname}/index.html`),
+      chunks: ["index"],
+      filename: "index.html",
     }),
   ],
 };
